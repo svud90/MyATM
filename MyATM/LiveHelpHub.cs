40f866c8c249757a0c7b105fb4141df61d1621e9
svud90 <svud90@gmail.com>
2017-02-20 16:38:32 +1000

    implemtn live chat using signalr

---
+++ MyATM/LiveHelpHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MyATM
+{
+    [Authorize]
+    public class LiveHelpHub : Hub
+    {
+        public void SendMessage(string message)
+        {
+            var username = Context.User.Identity.Name;
+            Clients.All.showMessage(username,message);
+        }
+    }
+}
